f67f40a66440948e73b3f159083dd1756fd87b67
aracheldog <zouzhicheng2001@outlook.com>
2024-04-19 14:03:38 -0400

    add

--- model/semseg/deeplabv3plus.py
+++ model/semseg/deeplabv3plus.py
@@ -4,12 +4,39 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+import torch
+from torch import nn
+import torch.nn.functional as F
+from model.semseg.base import BaseNet
+
+class SelfAttention(nn.Module):
+    def __init__(self, in_channels):
+        super(SelfAttention, self).__init__()
+        self.query_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels, in_channels // 8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels, in_channels, kernel_size=1)
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.size()
+        proj_query = self.query_conv(x).view(batch_size, -1, width * height).permute(0, 2, 1)
+        proj_key = self.key_conv(x).view(batch_size, -1, width * height)
+        energy = torch.bmm(proj_query, proj_key)
+        attention = F.softmax(energy, dim=-1)
+        proj_value = self.value_conv(x).view(batch_size, -1, width * height)
+
+        out = torch.bmm(proj_value, attention.permute(0, 2, 1))
+        out = out.view(batch_size, channels, height, width)
+        out = self.gamma * out + x
+        return out
 
 class DeepLabV3Plus(BaseNet):
     def __init__(self, backbone, nclass):
         super(DeepLabV3Plus, self).__init__(backbone)
 
+        # low_level_channels = 4 * 64 = 256
         low_level_channels = self.backbone.channels[0]
+        #  4 * 512 = 2058
         high_level_channels = self.backbone.channels[-1]
 
         self.head = ASPPModule(high_level_channels, (12, 24, 36))
@@ -17,6 +44,8 @@ class DeepLabV3Plus(BaseNet):
         self.reduce = nn.Sequential(nn.Conv2d(low_level_channels, 48, 1, bias=False),
                                     nn.BatchNorm2d(48),
                                     nn.ReLU(True))
+
+        self.attention = SelfAttention(48 + high_level_channels // 8)
 
         self.fuse = nn.Sequential(nn.Conv2d(high_level_channels // 8 + 48, 256, 3, padding=1, bias=False),
                                   nn.BatchNorm2d(256),
@@ -39,7 +68,11 @@ class DeepLabV3Plus(BaseNet):
 
         c1 = self.reduce(c1)
 
-        out = torch.cat([c1, c4], dim=1)
+        c1_and_c4 = torch.cat([c1, c4], dim=1)
+
+        c1_and_c4_attended = self.attention(c1_and_c4)
+
+        out = torch.cat([c1_and_c4, c1_and_c4_attended], dim=1)
         out = self.fuse(out)
 
         out = self.classifier(out)
