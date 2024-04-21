87510bf45db9da9766340a70c3fd2543a14162ba
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 03:27:25 -0400

    2

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from torch.optim import SGD
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 print("Number of available GPUs:", torch.cuda.device_count())
 
 MODE = None
