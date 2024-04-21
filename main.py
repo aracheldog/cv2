dc1a71d9e82aff222423d44aaff27d0eab83bc83
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 03:24:51 -0400

    add print

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from torch.nn import CrossEntropyLoss, DataParallel
 from torch.optim import SGD
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
 print("Number of available GPUs:", torch.cuda.device_count())
 
