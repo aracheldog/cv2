4afabbd7e9e97a17e4fad12fa4a3c0f2d6ad7076
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 09:22:10 -0400

    Automated commit

--- main.py
+++ main.py
@@ -221,8 +221,8 @@ def init_basic_elems(args):
                       'lr': args.lr * head_lr_multiple}],
                     lr=args.lr, momentum=0.9, weight_decay=1e-4)
 
-    # model = DataParallel(model)
-    model = DataParallel(args.first, model, dim=0)
+    model = DataParallel(model)
+    # model = BalancedDataParallel(args.first, model, dim=0)
     model.to(device)
 
 
