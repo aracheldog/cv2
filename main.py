915c91d317b6a5f6064829c48b40e767bb2470e2
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 07:54:40 -0400

    Automated commit

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ MODE = None
 
 
 def collate_fn(batch):
+  print(batch)
   return {
       'pixel_values': torch.stack([x['pixel_values'] for x in batch]),
       'labels': torch.tensor([x['labels'] for x in batch])
