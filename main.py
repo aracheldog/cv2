bf406eaae52680c891beb9e36351e3d563623847
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 10:23:12 -0400

    Automated commit

--- main.py
+++ main.py
@@ -430,7 +430,7 @@ class BalancedDataParallel(DataParallel):
             device_ids = self.device_ids[1:]
         else:
             device_ids = self.device_ids
-        print(len(inputs).size())
+        print(inputs[0].size())
         if len(inputs) == 1:
             return self.module(*inputs, **kwargs)
         inputs, kwargs = self.scatter(inputs, kwargs, device_ids)
