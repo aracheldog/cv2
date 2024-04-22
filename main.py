4f4c95009ac1c71376bb9b8a16e606a4252b698e
aracheldog <zouzhicheng2001@outlook.com>
2024-04-21 20:39:19 -0400

    Automated commit

--- main.py
+++ main.py
@@ -203,11 +203,12 @@ def train(model, trainloader, trainloader_u ,valloader, criterion, optimizer, ar
                 pred_u_pseudo = model(img_u_s).detach()
                 pseudo_label = pred_u_pseudo.argmax(dim=1)
 
-            model.to(device)
+
             model.train()
             num_lb, num_ulb = img_x.shape[0], img_u_s.shape[0]
-
-            preds = model(torch.cat((img_x, img_u_s)).to(device))
+            print(model.is_cuda())
+            print(torch.cat((img_x, img_u_s)).is_cuda())
+            preds = model(torch.cat((img_x, img_u_s)))
             pred_x, pred_u = preds.split([num_lb, num_ulb])
             loss_x = criterion(pred_x, mask_x)
             loss_u_w_fp = criterion(pred_u, pseudo_label)
